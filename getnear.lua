93e9bf0b0e7569cd2a71dd1737bc3ab1a054d3e3
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2025-05-21 02:16:06 -0700

    added getting all nearby

--- getnear.lua
+++ getnear.lua
@@ -98,4 +98,70 @@ else
         if not distance then distance = 10.0 end
         print(GetClosestObjectOfType(coords.x,coords.y,coords.z, distance, type, false, false, false))
     end
+    function ps.getNearbyPed(coords, distance)
+        if not coords then coords = GetEntityCoords(PlayerPedId()) end
+        if not distance then distance = 25.0 end
+        local pedList = GetGamePool('CPed')
+        local nearby = {}
+        for i = 1, #pedList do
+            local ped = pedList[i]
+            if ped ~= PlayerPedId() then
+                local pedCoords = GetEntityCoords(ped)
+                local dist = #(coords - pedCoords)
+                if dist < distance then
+                    table.insert(nearby, {ped = ped, distance = dist})
+                end
+            end
+        end
+        if #nearby > 0 then
+            return nearby
+        else
+            ps.notify('No Peds Nearby', 'error')
+            return {}
+        end
+    end
+    function ps.getNearbyVehicles(coords, distance)
+        if not coords then coords = GetEntityCoords(PlayerPedId()) end
+        if not distance then distance = 25.0 end
+        local vehicleList = GetGamePool('CVehicle')
+        local nearby = {}
+        for i = 1, #vehicleList do
+            local vehicle = vehicleList[i]
+            if vehicle ~= PlayerPedId() then
+                local vehicleCoords = GetEntityCoords(vehicle)
+                local dist = #(coords - vehicleCoords)
+                if dist < distance then
+                    table.insert(nearby, {vehicle = vehicle, distance = dist})
+                end
+            end
+        end
+        if #nearby > 0 then
+            return nearby
+        else
+            ps.notify('No Vehicles Nearby', 'error')
+            return {}
+        end
+    end
+    function ps.getNearbyObjects(coords, distance)
+        if not coords then coords = GetEntityCoords(PlayerPedId()) end
+        if not distance then distance = 25.0 end
+        local objectList = GetGamePool('CObject')
+        local nearby = {}
+        for i = 1, #objectList do
+            local object = objectList[i]
+            if object ~= PlayerPedId() then
+                local objectCoords = GetEntityCoords(object)
+                local dist = #(coords - objectCoords)
+                if dist < distance then
+                    table.insert(nearby, {object = object, distance = dist})
+                end
+            end
+        end
+        if #nearby > 0 then
+            return nearby
+        else
+            ps.notify('No Objects Nearby', 'error')
+            return {}
+        end
+    end
 end
