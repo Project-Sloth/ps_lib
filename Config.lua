a80090d7eb9c90dcbf9be9090ef7f3bca96e92a0
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2025-06-01 20:24:48 -0700

    - `edit`: removed Config.Framework and now it Checks if resource is started to load modules :)

--- Config.lua
+++ Config.lua
@@ -1,15 +1,18 @@
 Config = {}
-Config.Framework = "qb" -- qb or esx or qbx Needed for functions to work
+
 Config.Notify = "qb" -- QBCore or ESX or ox
 Config.Progressbar = "oxbar" -- QBCore or ESX or ox
 Config.Target = "qb" -- QBCore or ESX or ox or interact 
 Config.Inventory = "qb" -- QBCore or ESX or ox
 Config.Logs = "discord" -- fivemerr or discord
 ps = {}
-if Config.Framework == "qb" then
+
+QBCore, ESX, qbx = nil, nil, nil
+
+if GetResourceState('qb-core') == 'started' then
     QBCore = exports['qb-core']:GetCoreObject()
-elseif Config.Framework == "esx" then
+elseif GetResourceState('es-extended') then
     ESX = exports['es_extended']:getSharedObject()
-elseif Config.Framework == "qbx" then
+elseif GetResourceState('qbx_core') then
     qbx = exports.qbx_core
-end 
+end
